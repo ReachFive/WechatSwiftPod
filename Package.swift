2c70f8a8e3cc62f9e61487b5fb4739311fe7d728
François Devémy <francois.devemy@reach5.co>
2024-06-28 21:27:04 +0200

    Ajout Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WechatSwiftPod",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(name: "WechatSwiftPod", targets: ["WechatSwiftPod"]),
+    ],
+    targets: [
+        .target(name: "WechatSwiftPod", path: "WechatSwiftPod"),
+    ]
+)
